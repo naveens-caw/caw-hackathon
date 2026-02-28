a64bf9e2a1aae18d87ba23bc03e642a3c619a3f4
naveens-caw <naveen.s@caw.tech>
2026-02-28 21:43:49 +0530

    verify sigup page fix

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{ts,tsx,js,jsx}': ['eslint --fix', 'prettier --write', () => 'pnpm typecheck'],
+  '*.{ts,tsx,js,jsx}': ['eslint --fix', 'prettier --write', () => 'pnpm typecheck', () => 'pnpm test'],
   '*.{json,md,yaml,yml,css}': ['prettier --write'],
 };
